db52fa0ee44977ff8b9529d28c6ed1938eb51d03
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-08-28 17:11:23 +0200

    Verifica aggiuntiva sugli argomenti nel testJpaMapping dell'oggetto Book, Test Green con log

--- src/test/java/com/book/bookmanager/jpa/BookJpaTest.java
+++ src/test/java/com/book/bookmanager/jpa/BookJpaTest.java
@@ -1,6 +1,9 @@
 package com.book.bookmanager.jpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,6 +21,15 @@ public class BookJpaTest {
 
 	@Test
 	public void testJpaMapping() {
-		Book saved = entityManager.persistFlushFind(new Book(null, "test", "test", "test", 1000));
+		Book saved = entityManager.persistFlushFind(new Book(null, "test", "test", "test", 20));
+		assertThat(saved.getTitle()).isEqualTo("test");
+		assertThat(saved.getAuthor()).isEqualTo("test");
+		assertThat(saved.getCategory()).isEqualTo("test");
+		assertThat(saved.getPrice()).isEqualTo(20);
+		assertThat(saved.getId()).isNotNull();
+		assertThat(saved.getId()).isPositive();
+
+		// Per vedere identifier generato
+		LoggerFactory.getLogger(BookJpaTest.class).info("Saved: " + saved.toString());
 	}
 }
