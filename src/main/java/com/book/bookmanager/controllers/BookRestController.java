1413495e2b55d1c5487530f61d1389bbe4e285ea
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-01 17:42:55 +0200

    Implementazione minima del metodo che restituisce una lista vuota

--- src/main/java/com/book/bookmanager/controllers/BookRestController.java
+++ src/main/java/com/book/bookmanager/controllers/BookRestController.java
@@ -1,5 +1,19 @@
 package com.book.bookmanager.controllers;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.book.bookmanager.model.Book;
+
+@RestController
 public class BookRestController {
-	
+
+	@GetMapping("/api/employees")
+	public List<Book> allBooks() {
+		return Collections.emptyList();
+	}
+
 }
