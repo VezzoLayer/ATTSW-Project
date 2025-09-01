e76ce7c4246b04cb88f6c02247125cae3054ad88
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-01 19:25:28 +0200

    Refactor con RequestMapping

--- src/main/java/com/book/bookmanager/controllers/BookRestController.java
+++ src/main/java/com/book/bookmanager/controllers/BookRestController.java
@@ -5,23 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.book.bookmanager.model.Book;
 import com.book.bookmanager.services.BookService;
 
 @RestController
+@RequestMapping("/api/books")
 public class BookRestController {
 
 	@Autowired
 	private BookService bookService;
 
-	@GetMapping("/api/books")
+	@GetMapping
 	public List<Book> allBooks() {
 		return bookService.getAllBooks();
 	}
 
-	@GetMapping("/api/books/{id}")
+	@GetMapping("/{id}")
 	public Book oneBook(@PathVariable long id) {
 		return bookService.getBookById(id);
 	}
