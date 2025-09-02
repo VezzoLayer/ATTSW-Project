ac0b46f7dc96de0d5039c684f868bd8f5fee1716
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-02 21:04:20 +0200

    Modifica dipendenza pom htmlunit, test per titolo dell'home page e implementazione (green)

--- src/test/java/com/book/bookmanager/controllers/BookWebControllerHtmlUnitTest.java
+++ src/test/java/com/book/bookmanager/controllers/BookWebControllerHtmlUnitTest.java
@@ -1,5 +1,33 @@
 package com.book.bookmanager.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.htmlunit.WebClient;
+import org.htmlunit.html.HtmlPage;
+
+import com.book.bookmanager.services.BookService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = BookWebController.class)
 public class BookWebControllerHtmlUnitTest {
+
+	@Autowired
+	private WebClient webClient;
+
+	@MockitoBean
+	private BookService bookService;
+
+	@Test
+	public void testHomePageTitle() throws Exception {
+		HtmlPage page = webClient.getPage("/");
+		assertThat(page.getTitleText()).isEqualTo("Books");
+	}
 
 }
