d9b50711181140c87ccd871573786ec59be033b7
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-05 18:21:35 +0200

    Test get all books dopo averli inseriti nella repo (green)

--- src/it/java/com/book/bookmanager/BookRestControllerIT.java
+++ src/it/java/com/book/bookmanager/BookRestControllerIT.java
@@ -3,6 +3,7 @@ package com.book.bookmanager;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.contains;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -74,5 +75,21 @@ public class BookRestControllerIT {
 		given().accept(MediaType.APPLICATION_JSON_VALUE).when().get("/api/books/" + savedBook.getId()).then()
 				.statusCode(200).body("id", equalTo(savedBook.getId().intValue())).body("title", equalTo("title"))
 				.body("author", equalTo("author")).body("category", equalTo("category")).body("price", equalTo(10));
+	}
+
+	@Test
+	public void testGetAllBooks() {
+		// Inserisco alcuni libri nella repository
+		Book book1 = bookRepository.save(new Book(null, "title1", "author1", "category1", 10));
+		Book book2 = bookRepository.save(new Book(null, "title2", "author2", "category2", 20));
+		Book book3 = bookRepository.save(new Book(null, "title3", "author3", "category3", 30));
+
+		// Faccio la GET e verifico anche l'ordine
+		given().accept(MediaType.APPLICATION_JSON_VALUE).when().get("/api/books").then().statusCode(200)
+				.body("size()", equalTo(3))
+				.body("id", contains(book1.getId().intValue(), book2.getId().intValue(), book3.getId().intValue()))
+				.body("title", contains("title1", "title2", "title3"))
+				.body("author", contains("author1", "author2", "author3"))
+				.body("category", contains("category1", "category2", "category3")).body("price", contains(10, 20, 30));
 	}
 }
