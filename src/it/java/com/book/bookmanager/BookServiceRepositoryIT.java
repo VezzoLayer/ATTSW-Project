e7dfbc3cb390d5df5ab3e2de4c344b44736ada85
VezzoLayer <jacopovezzosiwork@gmail.com>
2025-09-05 13:14:07 +0200

    Configurazione postgre db, l'IT tra service e repository non usa h2 in memory, test service può inserire nella repo (Green)

---
+++ src/it/java/com/book/bookmanager/BookServiceRepositoryIT.java
@@ -0,0 +1,35 @@
+package com.book.bookmanager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.book.bookmanager.model.Book;
+import com.book.bookmanager.repositories.BookRepository;
+import com.book.bookmanager.services.BookService;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Import(BookService.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // usa Postgres, non H2 in-memory
+public class BookServiceRepositoryIT {
+
+	@Autowired
+	private BookService bookService;
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Test
+	public void testServiceCanInsertIntoRepository() {
+		Book savedBook = bookService.insertNewBook(new Book(null, "book", "author", "category", 10));
+
+		assertThat(bookRepository.findById(savedBook.getId())).isPresent();
+	}
+}
